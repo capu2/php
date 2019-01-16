f53828ed5e05f480182fc91b5573f4096fb02cb2
Mestre <rscosta1990@gmail.com>
2019-01-16 15:30:28 -0300

    resolvi a merda de verdade

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 
 
-<<<<<<< HEAD
+
 	echo "Olá Mestre";
 
 	echo "VocÊ é o cara!";
-=======
-	echo "Olá Mestre!!";
 
-	echo "Você é o melhor!";
->>>>>>> 7a5012a040b6a8fb6a78f910fc50a7b85a79c8b0
 
 ?>
